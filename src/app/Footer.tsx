f06838200d91d5c9d22b1b70d8b482a07e86c63b
kirikirisu <b1801815@planet.kanazawa-it.ac.jp>
2024-03-09 10:04:10 +0900

    footer

--- src/app/Footer.tsx
+++ src/app/Footer.tsx
@@ -2,6 +2,9 @@
 
 import { Editor } from "@tiptap/react";
 import { ChangeEvent, useRef } from "react";
+import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
+import { PictureIcon } from "./icons/PictureIcon";
+import "react-circular-progressbar/dist/styles.css";
 
 const loadImage = (file: File) => {
   return new Promise<HTMLImageElement>((resolve) => {
@@ -11,7 +14,7 @@ const loadImage = (file: File) => {
   });
 };
 
-export function Footer({ editor }: { editor: Editor | null }) {
+export function Footer({ editor }: { editor: Editor }) {
   const inputRef = useRef<HTMLInputElement | null>(null);
 
   const handleOnChange = async (event: ChangeEvent<HTMLInputElement>) => {
@@ -57,11 +60,18 @@ export function Footer({ editor }: { editor: Editor | null }) {
     }
   };
 
+  const charCount = editor.storage.characterCount.characters() as number;
+  const charPercentage = (charCount / 14000) * 100;
+
   return (
-    <>
+    <div className="footer">
       <div>
-        <button type="button" onClick={handleOnClcik}>
-          <span>Insert Image</span>
+        <button
+          type="button"
+          onClick={handleOnClcik}
+          className="picture-icon-button"
+        >
+          <PictureIcon color="#a7abb1" width="1.25rem" height="1.25rem" />
         </button>
         <input
           className="insert-image-input"
@@ -71,7 +81,31 @@ export function Footer({ editor }: { editor: Editor | null }) {
           onChange={handleOnChange}
         />
       </div>
-      <div>{editor?.storage.characterCount.characters()}</div>
-    </>
+      <button
+        type="button"
+        style={{
+          width: 35,
+          height: 35,
+          display: "block",
+          backgroundColor: "white",
+          borderWidth: 0,
+          margin: 0,
+          padding: 0,
+          cursor: "pointer",
+        }}
+      >
+        <CircularProgressbar
+          value={charPercentage}
+          text={charCount.toString()}
+          strokeWidth={6}
+          styles={buildStyles({
+            pathColor: "#696f73",
+            trailColor: "#d8dadf",
+            textColor: "rgb(141, 146, 152)",
+            textSize: "22",
+          })}
+        />
+      </button>
+    </div>
   );
 }
